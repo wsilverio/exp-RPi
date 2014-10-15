2d8db54571ebbc5c2c9bc4c22864c1de955d727b
wsilverio <wendeurick@gmail.com>
2014-10-15 17:24:23 -0300

    Programas em Python

---
+++ Python/debouncing/README.md
@@ -0,0 +1,6 @@
+Debouncing  
+  
+botao 'ESQUERDA' conectado ao pino board(13) = bcm(21)  
+botao 'DIREITA' conectado ao pino board(11) = bcm(17)  
+botao 'ESQ' conectado ao pino board(15) = bcm(22)  
+botao 'ENTER' conectado ao pino board(7) = bcm(4)  
---
+++ Python/debouncing/debouncing.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+from time import sleep
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+'''
+Botoes conectados nos pinos:
+BCM 4 = BOARD pin 7
+BCM 17 = BOARD pin 11
+BCM 21 = BOARD pin 13
+BCM 22 = BOARD pin 15
+'''
+BOTAO_ESQUERDA = 21 
+BOTAO_DIREITA = 17
+BOTAO_ENTER = 4
+BOTAO_ESC= 22
+
+GPIO.setup(BOTAO_ESQUERDA, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(BOTAO_DIREITA, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(BOTAO_ENTER, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(BOTAO_ESC, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+debouncing = 0.05
+
+while True:
+	if GPIO.input(BOTAO_ESQUERDA):
+		print "Esquerda"
+		sleep(debouncing)
+		while GPIO.input(BOTAO_ESQUERDA): pass
+	elif GPIO.input(BOTAO_DIREITA):
+		print "Direita"
+		sleep(debouncing)
+		while GPIO.input(BOTAO_DIREITA): pass
+	elif GPIO.input(BOTAO_ENTER):
+		print "Enter"
+		sleep(debouncing)
+		while GPIO.input(BOTAO_ENTER): pass
+	elif GPIO.input(BOTAO_ESC):
+		print "Esc"
+		sleep(debouncing)
+		while GPIO.input(BOTAO_ESC): pass
+	
+	sleep(debouncing)
