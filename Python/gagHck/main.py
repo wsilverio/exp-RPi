43c4df3d53065c2354e928fb5fb23153c7133097
Wendeu <wendeurick@gmail.com>
2015-01-30 22:06:05 -0200

    add funcao pisca

--- Python/gagHck/main.py
+++ Python/gagHck/main.py
@@ -2,6 +2,7 @@
 
 import sys
 import RPi.GPIO as GPIO
+from time import sleep
 
 pin, std = int(sys.argv[1]), int(sys.argv[2])
 
@@ -11,4 +12,15 @@ GPIO.setmode(GPIO.BOARD)
 
 GPIO.setup(pin, GPIO.OUT)
 
-GPIO.output(pin, std)
+#GPIO.output(pin, std)
+
+if std > 1 :
+	GPIO.output(pin,0)
+	sleep(0.5)
+	for i in xrange(std):
+		GPIO.output(pin,True)
+		sleep (0.1)
+		GPIO.output(pin,False)
+		sleep (0.1)
+else:
+	GPIO.output(pin, std)
