bdf36cad8a643670c97aa4f1eb529aef9635ca3b
Silverio <obelonave@gmail.com>
2017-03-24 00:20:21 -0300

    teste

--- sala-intensidade/autoinstall.sh
+++ sala-intensidade/autoinstall.sh
@@ -3,9 +3,12 @@
 PACOTES_APT="alsa-utils mpg123 python-dev python-rpi.gpio python-pip"
 PACOTES_PIP="pyfirmata"
 
-sudo apt-get update -y
-sudo apt-get install -y $PACOTES_APT
-sudo pip install $PACOTES_PIP
+# sudo apt-get update -y
+# sudo apt-get install -y $PACOTES_APT
+# sudo pip install $PACOTES_PIP
 
-sudo modprobe snd_bcm2835
-sudo amixer cset numid=3 1
+# sudo modprobe snd_bcm2835
+# sudo amixer cset numid=3 1
+
+echo $PACOTES_PIP
+echo $PACOTES_APT
