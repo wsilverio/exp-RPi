7f5d54376a209a4197e4dbb1f103075b4fd50eb7
Silverio <obelonave@gmail.com>
2017-03-24 00:12:55 -0300

    criado autoinstall test

---
+++ sala-intensidade/autoinstall.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PACOTES_APT = "alsa-utils mpg123 python-dev python-rpi.gpio python-pip"
+PACOTES_PIP = "pyfirmata"
+
+sudo apt-get update -y
+sudo apt-get install -y $PACOTES_APT
+sudo pip install $PACOTES_PIP
+
+sudo modprobe snd_bcm2835
+sudo amixer cset numid=3 1
