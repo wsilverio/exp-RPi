6f352ccd578d62b4cc259a8dec766a861fee2d0f
Silverio <obelonave@gmail.com>
2017-03-24 00:15:18 -0300

    espacossss

--- sala-intensidade/autoinstall.sh
+++ sala-intensidade/autoinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PACOTES_APT = "alsa-utils mpg123 python-dev python-rpi.gpio python-pip"
-PACOTES_PIP = "pyfirmata"
+PACOTES_APT="alsa-utils mpg123 python-dev python-rpi.gpio python-pip"
+PACOTES_PIP="pyfirmata"
 
 sudo apt-get update -y
 sudo apt-get install -y $PACOTES_APT
